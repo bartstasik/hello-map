758b8875c800f1af6c3180dcb01e3f9378b1515e
Bartosz Stasik <bartolus99@gmail.com>
2019-10-25 18:01:49 +0100

    project: create new HelloWorld project

---
+++ HelloWorld/Assets/Scripts/Bomb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Bomb : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Respawn();
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        var randomX = Random.Range(-10, 10);
+        var randomY = Random.Range(10, 20);
+        transform.position = new Vector3(randomX, randomY);
+        rigidbody.velocity = Vector3.zero;
+    }
+}
---
+++ HelloWorld/Assets/Scripts/CubeMover.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+public class CubeMover : MonoBehaviour
+{
+    
+    [SerializeField]
+    public GameObject myPrefab;
+    
+    [SerializeField]
+    private float speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical);
+
+        transform.position += speed * Time.deltaTime * movement;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
