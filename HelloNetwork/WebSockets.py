c81f97d2a4808f2651d5da788f3c361b803bc580
Bartosz Stasik <bartolus99@gmail.com>
2020-06-15 10:06:14 +0100

    nn: upload all training data
    After recording data from multiple participants on multiple maps, import
    all of it under `data_fixed_ray`. This is picked up by a file crawler in
    the code.

--- HelloNetwork/WebSockets.py
+++ HelloNetwork/WebSockets.py
@@ -5,9 +5,26 @@ import numpy as np
 import tensorflow_core as tf
 import tensorflow_core.python.keras.backend as kb
 import websockets
+<<<<<<< Updated upstream
 from tensorflow_core.python.keras import Model
 from tensorflow_core.python.keras.layers import Dense, LSTM, GaussianNoise, Activation, Input, \
     GaussianDropout
+=======
+import os
+import math
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+from tensorflow_core.python.client import device_lib
+from tensorflow_core.python.keras import Sequential, Model
+from tensorflow_core.python.keras.callbacks import EarlyStopping
+from tensorflow_core.python.keras.layers import Dense, LSTM, GaussianNoise, Activation, BatchNormalization, Input, \
+    concatenate, GaussianDropout, CuDNNLSTM
+
+import nn_message_pb2 as pb
+
+print(tf.test.is_built_with_cuda())
+print(device_lib.list_local_devices())
+>>>>>>> Stashed changes
 
 
 def convert_function(n):
@@ -23,16 +40,74 @@ def custom_loss(x0, x):
     term_b = c_b * kb.log(b)
     result = -0.5 * (term_a + term_b)
     return result
+<<<<<<< Updated upstream
+=======
+
+
+def lstm_simple():
+    # inputs = inputs.reshape((1, input_timesteps, input_num))
+    # labels = labels.reshape((1, label_timesteps, label_num))
+
+    model = Sequential()
+
+    # model.add(Dense(16, activation=relu, input_shape=(10,)))
+    model.add(LSTM(6, stateful=True, return_sequences=True, batch_input_shape=(1, None, 7)))
+    # model.add(Dense(128, activation='relu'))
+    # model.add(SimpleRNN(128))
+    model.add(Dense(2, activation='tanh'))
+
+    model.load_weights('model.h5')
+
+    model.compile(optimizer='sgd',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    return model
+
+
+def lstm_simple_noisy():
+    # inputs = inputs.reshape((1, input_timesteps, input_num))
+    # labels = labels.reshape((1, label_timesteps, label_num))
+
+    model = Sequential()
+
+    # model.add(Dense(16, activation=relu, input_shape=(10,)))
+    model.add(LSTM(6, stateful=True, return_sequences=True, batch_input_shape=(1, None, 7), unit_forget_bias=True))
+    # model.add(Dense(128, activation='relu'))
+    # model.add(SimpleRNN(128))
+    model.add(GaussianNoise(0.5))
+    model.add(Activation('tanh'))
+    # model.add(BatchNormalization())
+    model.add(Dense(2, activation='tanh'))
+
+    model.load_weights('model.h5')
+
+    model.compile(optimizer='sgd',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    return model
+>>>>>>> Stashed changes
 
 
 def lstm_functional():
     input = Input(batch_input_shape=(1, None, 7))
-    layer_1 = LSTM(6, stateful=True, return_sequences=True)(input)
+    layer_1 = CuDNNLSTM(7, stateful=True, return_sequences=True)(input)
     layer_1 = GaussianNoise(0.5)(layer_1)
     layer_1 = Activation('tanh')(layer_1)
+<<<<<<< Updated upstream
     layer_1 = GaussianDropout(0.2)(layer_1)
     output_mouse = Dense(1, activation='tanh', name='mouse', )(layer_1)
     output_w_key = Dense(1, activation='relu', name='w_key')(layer_1)
+=======
+    # layer_1 = GaussianDropout(0.2)(layer_1)
+    output_mouse = Dense(1, activation='tanh', name='mouse', )(layer_1)
+    output_w_key = Dense(1, activation='tanh', name='w_key')(layer_1)
+>>>>>>> Stashed changes
 
     model = Model(inputs=input, outputs=[output_mouse, output_w_key])
 
@@ -52,6 +127,24 @@ def lstm_functional():
     return model
 
 
+def dense_3d():
+    model = Sequential()
+
+    model.add(Dense(6, activation='relu', batch_input_shape=(1, None, 7)))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(2, activation='tanh'))
+
+    model.load_weights('model.h5')
+
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    return model
+
+
 async def echo(websocket, path):
     async for message in websocket:
         a = pb.MovementInput()
@@ -64,6 +157,19 @@ async def echo(websocket, path):
                             a.eastRay,
                             a.westRay,
                             a.distanceFromPlayer]], dtype=float)
+<<<<<<< Updated upstream
+=======
+        # inputs = np.array([[a.rotation,
+        #                     a.northRay,
+        #                     a.northwestRay,
+        #                     a.northeastRay,
+        #                     a.eastRay,
+        #                     a.southRay,
+        #                     a.westRay,
+        #                     a.doorClosed,
+        #                     a.distanceFromPlayer,
+        #                     a.seesKey]], dtype=float)
+>>>>>>> Stashed changes
 
         inputs = inputs.reshape((1, 1, inputs.size))
 
@@ -78,12 +184,28 @@ async def echo(websocket, path):
         movement_output.rightButtonPressed = False  # round(output[4])
         movement_output.keyButtonPressed = False  # round(output[5])
 
+<<<<<<< Updated upstream
+=======
+        # print(output)
+
+>>>>>>> Stashed changes
         await websocket.send(movement_output.SerializeToString())
 
 
 tf.keras.backend.set_learning_phase(0)
 
+<<<<<<< Updated upstream
 pre_model = lstm_functional()
+=======
+# pre_model = tf.keras.models.load_model('rnn tanh bigger data 75 percent.h5')
+# pre_model = tf.keras.models.load_model('model.h5')
+# pre_model = lstm_simple_noisy()
+# pre_model = dense_3d()
+pre_model = lstm_functional()
+inputs = [[[0, 0, 0, 0, 0, 0, 0]]]
+pre_model.predict(inputs)
+# pre_model = lstm_simple()
+>>>>>>> Stashed changes
 
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(echo, 'localhost', 6969))
